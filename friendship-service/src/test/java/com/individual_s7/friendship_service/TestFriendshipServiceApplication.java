667be7da82c11aa71995e0ed72334a06565db092
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-09-20 15:24:35 +0200

    first-commit

---
+++ friendship-service/src/test/java/com/individual_s7/friendship_service/TestFriendshipServiceApplication.java
@@ -0,0 +1,11 @@
+package com.individual_s7.friendship_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFriendshipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FriendshipServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
